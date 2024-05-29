4cf96600e51728bd5bcafeadf7a365add6552140
jtlandis <jtlandis314@gmail.com>
2024-05-29 14:04:34 -0400

    adding some in-line reporting. Accessing meta data before it is set in filter.

--- _extensions/wordcount/words.lua
+++ _extensions/wordcount/words.lua
@@ -1,19 +1,35 @@
 
+as_str = function(meta_value, name)
+  if (meta_value == nil) then
+    print("cannot find " .. name .. " in meta data")
+    return pandoc.Null()
+  end
+  return pandoc.Str(meta_value) 
+end
+
+as_num = function(meta_value, name)
+  if (meta_value == nil) then
+    print("cannot find " .. name .. " in meta data")
+    return 0
+  end
+  return meta_value
+end
+
 return {
   ['words-body'] = function(args, kwargs, meta)
-    return pandoc.Str(meta.wordcount_body_words)
+    return as_str(meta.wordcount_body_words, "wordcount_body_words")
   end,
   ['words-ref'] = function(args, kwargs, meta)
-    return pandoc.Str(meta.wordcount_ref_words)
+    return as_str(meta.wordcount_ref_words, "wordcount_ref_words")
   end,
   ['words-append'] = function(args, kwargs, meta)
-    return pandoc.Str(meta.wordcount_appendix_words)
+    return as_str(meta.wordcount_appendix_words, "wordcount_appendix_words")
   end,
   ['words-note'] = function(args, kwargs, meta)
-    return pandoc.Str(meta.wordcount_note_words)
+    return as_str(meta.wordcount_note_words, "wordcount_note_words")
   end,
   ['words-total'] = function(args, kwargs, meta)
-    return pandoc.Str(meta.wordcount_total_words)
+    return as_str(meta.wordcount_total_words, "wordcount_total_words")
   end,
   ['words-sum'] = function(args, kwargs, meta)
     local nargs = #args
@@ -25,16 +41,16 @@ return {
     local arg = args[1][1].text
     print(arg)
     if arg:match("body") then
-      count = count + meta.wordcount_body_words
+      count = count + as_num(meta.wordcount_body_words, "wordcount_total_words")
     end
     if arg:match("ref") then
-      count = count + meta.wordcount_ref_words
+      count = count + as_num(meta.wordcount_ref_words, "wordcount_ref_words")
     end
     if arg:match("append") then
-      count = count + meta.wordcount_appendix_words
+      count = count + as_num(meta.wordcount_appendix_words, "wordcount_appendix_words")
     end
     if arg:match("note") then
-      count = count + meta.wordcount_note_words
+      count = count + as_num(meta.wordcount_note_words, "wordcount_note_words")
     end
     return pandoc.Str(count - nargs)
   end
