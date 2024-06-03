2130f191e74cd41bd3e2192213f9af776ee05eb4
Andrew Heiss <andrew@andrewheiss.com>
2024-06-03 00:24:52 -0400

    Add tests for no count sections

---
+++ tests/testthat/test-no-count.R
@@ -0,0 +1,15 @@
+test_that("`no-count` sections don't count words", {
+  test_file <- test_file_parts(here::here("tests/testthat/test-no-count.qmd"))
+  
+  create_local_quarto_project(test_file = test_file)
+  
+  quarto::quarto_render(input = test_file$qmd, quiet = TRUE)
+  
+  counts <- get_wordcounts(test_file$md)
+  
+  expect_equal(counts$wordcount_appendix_words, 0)
+  expect_equal(counts$wordcount_body_words, 4)
+  expect_equal(counts$wordcount_note_words, 0)
+  expect_equal(counts$wordcount_ref_words, 0)
+  expect_equal(counts$wordcount_total_words, 4)
+})
